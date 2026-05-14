df8e76ca4eed88dad491dc9a5c53933564780815
bebrita123 <mangowillgoodboy@gmail.com>
2026-05-14 17:57:24 +0700

    изменен префикс для корректной работы на сервере

--- app/dependencies.py
+++ app/dependencies.py
@@ -7,7 +7,7 @@ from app.db.session import get_db
 from app.users.crud import get_user
 from app.users.model import Role, User
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/auth/login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/v1/auth/login")
 
 
 async def get_current_user(
