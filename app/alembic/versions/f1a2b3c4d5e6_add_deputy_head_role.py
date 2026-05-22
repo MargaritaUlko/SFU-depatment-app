9902e65496d2bee9b3f4ea4eaea0d9e036dddedd
bebrita123 <mangowillgoodboy@gmail.com>
2026-05-22 15:02:25 +0700

    добавлена роль зав кафедрой-

--- app/alembic/versions/f1a2b3c4d5e6_add_deputy_head_role.py
+++ app/alembic/versions/f1a2b3c4d5e6_add_deputy_head_role.py
@@ -11,7 +11,7 @@ from typing import Sequence, Union
 from alembic import op
 
 revision: str = "f1a2b3c4d5e6"
-down_revision: Union[str, None] = "aa3f86f1e167"
+down_revision: Union[str, None] = "e2f3a4b5c6d7"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
